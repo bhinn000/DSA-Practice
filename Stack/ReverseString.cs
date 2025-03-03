cd5c4c89736bb4c5ca704f89eeb5ab745ef72fd7
Bhintuna <bhintunash@gmail.com>
2025-03-03 11:20:58 +0545

    In the process of push(incomplete)

--- Stack/ReverseString.cs
+++ Stack/ReverseString.cs
@@ -3,43 +3,69 @@
 
 class ReverseString
 {
-    string sentence= " I  am  Bh";
+    //string sentence= " I  am  Bh";
+    string sentence = "I am Bhintuna";
     //void InputString()
     //{
     //    Console.WriteLine("Say Something");
     //    sentence=Console.ReadLine();
     //}
 
-    void CountWords()
+    //int[] myStack = new int[5];
+    List<char> myStack = new List<char>();
+    int top = -1;
+    //int capacity;//length of the words
+
+    void Push(char charac) //to push the character to the stack
+    {
+            myStack.Add(charac);
+            top++;
+        
+    }
+
+    int CountWords(string sentence)
     {
         int count = 0;
-        string trimmedSentence=sentence.Trim();
-        
+        string trimmedSentence = sentence.Trim();
+
         for (int charac = 0; charac < trimmedSentence.Length; charac++)
         {
             if (trimmedSentence[charac] == ' ' && trimmedSentence[charac+1] != ' ')
             {
                 count++;
+               
             }
-
         }
-        Console.WriteLine($"Here , the count is {count+1}");
+        return count;
     }
 
-    void DisplayEachWords()
+    int StackMaxCapacity(string sentence) // to define the capacity of the stack depending upon the largest requirement , if array had been used
+    {
+        string trimmedSentence = sentence.Trim();
+        string[] words = trimmedSentence.Split(" ");
+
+        List<int> eachWordsLength =words.Select(word => word.Length).ToList();
+        eachWordsLength.ForEach(wordLength=>Console.WriteLine($"Its length is {wordLength}"));
+        int capacity = eachWordsLength.Max();
+        return capacity;
+    }
+    
+    void PushEachWordInStack()
     {
 
     }
-    
+
     //"I am Bhintuna"
     //" I am Bhintuna " : leading and trailing space around the sentence
     //"I  am  Bhintuna" : double space 
-
+    
 
     static void Main(string[] args)
     {
         ReverseString reverseString = new ReverseString();
-        reverseString.CountWords();
+        int wordCount = reverseString.CountWords(reverseString.sentence);
+        int stackCapacity=reverseString.StackMaxCapacity(reverseString.sentence);
+        reverseString.Push('a');
     }
 
 
